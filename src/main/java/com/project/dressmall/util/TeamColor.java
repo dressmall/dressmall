49019cc9fcf87d10292901c301fdc8fcf8151596
orukdoline <skynets@kakao.com>
2024-11-25 15:14:27 +0900

    log 색상코드 추가

---
+++ src/main/java/com/project/dressmall/util/TeamColor.java
@@ -0,0 +1,8 @@
+package com.project.dressmall.util;
+
+public class TeamColor {
+	public static String KIM = "\u001B[34m";
+	public static String PARK = "\u001B[43m";
+	public static String JIN = "\u001B[41m";
+	public static String RESET = "\u001B[0m";
+}
