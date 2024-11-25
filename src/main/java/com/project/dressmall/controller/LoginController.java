3454ab952dd74b4af7e88667523ee9d7f37511f8
hyerin716 <hyerin71699@gmail.com>
2024-11-25 12:31:22 +0900

    css 적용 테스트

--- src/main/java/com/project/dressmall/controller/LoginController.java
+++ src/main/java/com/project/dressmall/controller/LoginController.java
@@ -1,8 +1,15 @@
 package com.project.dressmall.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class LoginController {
-
+	@GetMapping("/on/test")
+	public String getMethodName() {
+		return "on/test";
+	}
+	
 }
