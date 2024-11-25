00c636e0945535188d69cb396dec14d05732ebf1
hyerin716 <hyerin71699@gmail.com>
2024-11-25 15:03:45 +0900

    김혜린) staff on/off interceptor 추가, staff vo추가

--- src/main/java/com/project/dressmall/OffInterceptor.java
+++ src/main/java/com/project/dressmall/OffInterceptor.java
@@ -2,37 +2,28 @@ package com.project.dressmall;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Component 
-public class OnInterceptor implements HandlerInterceptor {
-	
-	// 특정 컨트롤러 실행 전에 request,response를 가로채어 먼저 실행
+@Component
+public class OffInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		
+		log.debug(request.getRequestURI().toString()+"요청 Interceptor");
 		
-		log.debug(request.getRequestURL().toString() + "요청 Interceptor");
-		
-		// 로그인 하지 않았다면 session.getAttribute("loginStaff")
+		// 로그인이 되어 있다면 
 		HttpSession session = request.getSession();
-		if(session.getAttribute("loginStaff") == null) {
-			response.sendRedirect(request.getContextPath() + "/off/staff/login"); // off/staff/login.jsp
-			
+		if(session.getAttribute("loginStaff") != null) {
+			response.sendRedirect(request.getContextPath() + "/on/staff/staffList");	
 			return false;
 		}
 		
-		
-		
 		return HandlerInterceptor.super.preHandle(request, response, handler);
 	}
-	
 }
--- src/main/java/com/project/dressmall/vo/Staff.java
+++ src/main/java/com/project/dressmall/vo/Staff.java
@@ -1,5 +1,14 @@
 package com.project.dressmall.vo;
 
-public class Staff {
+import lombok.Data;
 
+@Data
+public class Staff {
+	
+	private Integer staffNo;
+	private String staffId;
+	private String staffPw;
+	private String updateDate;
+	private String createDate;
+	
 }
