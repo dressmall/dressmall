16605b382793635774f1e835fc9482d3293600b3
orukdoline <skynets@kakao.com>
2024-11-25 11:06:48 +0900

    테스트

---
+++ src/main/java/com/project/dressmall/DressmallApplication.java
@@ -0,0 +1,13 @@
+package com.project.dressmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressmallApplication.class, args);
+	}
+
+}
