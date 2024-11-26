435107528c7e3be5f43966dfa2d3bea27b443f26
orukdoline <skynets@kakao.com>
2024-11-26 16:01:16 +0900

    진수우) 스태프관리페이지 스태프출력기능 추가, 스태프 관리페이지 스태프추가기능 추가

--- src/main/java/com/project/dressmall/mapper/StaffMapper.java
+++ src/main/java/com/project/dressmall/mapper/StaffMapper.java
@@ -1,5 +1,8 @@
 package com.project.dressmall.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.project.dressmall.vo.Staff;
@@ -9,4 +12,13 @@ public interface StaffMapper {
 	
 	// staffLogin.jsp
 	Staff staffLogin(Staff staff);
+	
+	// /on/staff/staffList : 관리자페이지에서 스태프리스트 출력
+	List<Staff> selectStaffList(Map<String, Object> param);
+	
+	// /on/staff/staffList - 관리자페이지에서 스태프리스트 카운트.
+	Integer countStaffList();
+	
+	// /on/staff/addStaff : 관리자페이지에서 스태프 추가.
+	Integer insertStaff(Staff staff);
 }
